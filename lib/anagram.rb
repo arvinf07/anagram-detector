47017d3eee4f83765d0f78f043559cba3b445943
Arvin Fernandez <arvinf07@gmail.com>
2020-10-30 11:34:52 -0400

    commit tree b4953366682ad27d127e512ee33331dc4319289d

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  def initialize(word)
+    @word = word
+  end  
+
+  attr_accessor :word
+
+  def match(possible_matches)
+    # matches = []
+    possible_matches.select do |word|
+      word.split('').sort == @word.split('').sort  
+    end  
+  end 
+end
